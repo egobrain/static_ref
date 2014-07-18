3ca0585e81db337dd3f48f3de7ec731ee2724562
Kozlov Yakov <yk@exante.eu>
2014-07-18 13:51:43 +0400

    Validators generator

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+0.1.1
+===
+
+Features:
+---
+
+- Option valiators for each ref description. Now it can generate
+K/V validators: ```valid_RefName_key/1``` and ```valid_RefName_value/1```.
--- src/static_ref.erl
+++ src/static_ref.erl
@@ -6,6 +6,10 @@
          compile/2
         ]).
 
+-record(opts, {
+          validators = false
+         }).
+
 %% =============================================================================
 %%% API
 %% =============================================================================
@@ -16,13 +20,16 @@
       K :: any(),
       V :: any().
 compile(Module, Description) ->
-
     UnknownClause =
         ?clause([?underscore], none, [?error(?atom(unknown))]),
+
+    Description2 = lists:map(fun opts/1, Description),
+
     Funs =
         lists:flatten(
           [
-           [?function(
+           [
+            ?function(
                from(RefName),
                [
                 ?clause([?abstract(K)], none, [?ok(?abstract(V))])
@@ -33,8 +40,12 @@ compile(Module, Description) ->
                [
                 ?clause([?abstract(V)], none, [?ok(?abstract(K))])
                 || {K, V} <- Data
-               ] ++ [UnknownClause])
-           ] || {RefName, Data} <- Description
+               ] ++ [UnknownClause]),
+            case Opts#opts.validators of
+                true -> validators(RefName, Data);
+                false -> []
+            end
+           ] || {RefName, Data, Opts} <- Description2
           ]),
     Ast =
         [
@@ -47,15 +58,45 @@ compile(Module, Description) ->
     {module, Module} = code:load_binary(Module, "nofile", BinaryCode),
     ok.
 
+validators(RefName, Data) ->
+    UnknownClause =
+        ?clause([?underscore], none, [?error(?atom(unknown))]),
+    [
+     ?function(
+         valid_key(RefName),
+         [
+          ?clause([?abstract(K)], none, [?atom(ok)])
+          || {K, _V} <- Data
+         ] ++ [UnknownClause]),
+     ?function(
+        valid_value(RefName),
+        [
+         ?clause([?abstract(V)], none, [?atom(ok)])
+         || {_K, V} <- Data
+        ] ++ [UnknownClause])
+    ].
+
 %% =============================================================================
 %%% Internal functions
 %% =============================================================================
+
+
+opts({RefName, Data}) -> {RefName, Data, #opts{}};
+opts({RefName, Data, Opts}) ->
+    Opts2 = lists:foldl(fun set_opt/2, #opts{}, Opts),
+    {RefName, Data, Opts2}.
+
+set_opt(validators, Opts) -> Opts#opts{validators=true};
+set_opt(_, Opts) -> Opts.
 
 binary_to_atom(Bin) ->
     list_to_atom(binary_to_list(Bin)).
 
 from(RefName) -> binary_to_atom(<<"from_", RefName/binary>>).
 to(RefName) -> binary_to_atom(<<"to_", RefName/binary>>).
+
+valid_key(RefName) -> binary_to_atom(<<"valid_", RefName/binary, "_key">>).
+valid_value(RefName) -> binary_to_atom(<<"valid_", RefName/binary, "_value">>).
 
 %% =============================================================================
 %%% Tests
@@ -71,29 +112,74 @@ compile_test_() ->
          [
           {blocked, 1},
           {full_access, 2}
-         ]},
+         ], []},
     Ref2 =
         {<<"ref">>,
          [
           {1, a},
           {2, b},
           {3, c}
-         ]},
+         ], [validators]},
     Refs = [Ref1, Ref2],
     lists:flatten(
       [
-       {"Init test", fun() -> ok = static_dict:compile(Module, Refs) end},
+       {"Init test",
+        fun() ->
+                ok = static_ref:compile(Module, Refs),
+                ?assertEqual({file, "nofile"}, code:is_loaded(Module))
+        end},
        [
         [
          fun() -> ?assertEqual({ok, V}, Module:(from(RefName))(K)) end,
          fun() -> ?assertEqual({ok, K}, Module:(to(RefName))(V)) end
-        ] || {RefName, Data} <- Refs, {K, V} <- Data
+        ] || {RefName, Data, _} <- Refs, {K, V} <- Data
        ],
        [
         [
          fun() -> ?assertEqual({error, unknown}, Module:(from(RefName))(z)) end,
          fun() -> ?assertEqual({error, unknown}, Module:(to(RefName))(z)) end
-        ] || {RefName, _Data} <- Refs
+        ] || {RefName, _Data, _} <- Refs
+       ],
+       %% Validators
+       [
+        [
+         [
+          fun() ->
+                  ?assertEqual(
+                     false,
+                     erlang:function_exported(Module, valid_key(RefName), 1)),
+                  ?assertEqual(
+                     false,
+                     erlang:function_exported(Module, valid_value(RefName), 1))
+          end
+         ] || {RefName, _Data, _Opts} <- [Ref1]
+        ],
+        [
+         [
+          fun() ->
+                  ?assertEqual(
+                     true,
+                     erlang:function_exported(Module, valid_key(RefName), 1)),
+                  ?assertEqual(
+                     true,
+                     erlang:function_exported(Module, valid_value(RefName), 1))
+          end,
+          [
+           fun() ->
+                   ?assertEqual(ok, Module:(valid_key(RefName))(K)),
+                   ?assertEqual(ok, Module:(valid_value(RefName))(V))
+           end  || {K, V} <- Data
+          ],
+          fun() ->
+                  ?assertEqual(
+                     {error, unknown},
+                     Module:(valid_key(RefName))(z)),
+                  ?assertEqual(
+                     {error, unknown},
+                     Module:(valid_value(RefName))(z))
+          end
+         ] || {RefName, Data, _Opts} <- [Ref2]
+        ]
        ]
       ]).
 
